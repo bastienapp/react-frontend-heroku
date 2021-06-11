21a923bbca492e6f48203c5f10deb15afc490941
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-06-11 11:21:14 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import RemoveMe from './components/RemoveMe';
+import axios from 'axios';
 
 function App() {
+  const handleClick = () => {
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/signup`, {
+        email: 'test@mail.com',
+        password: 'soleil1234',
+      })
+      .then((response) => alert(JSON.stringify(response)));
+  };
+
   return (
     <div className="App">
-      <RemoveMe />
+      <button onClick={handleClick} type="button">
+        Click me!
+      </button>
     </div>
   );
 }
